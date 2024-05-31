fd329d5c3f42dd2ec0a38bf8e748d1beb7e023d8
simplyKien <155982765+simplyKien@users.noreply.github.com>
2024-05-31 23:20:30 +0700

    Add files via upload

---
+++ Carotest/Form2.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace Carotest
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Sitka Small", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.Firebrick;
+            label1.Location = new Point(80, 39);
+            label1.Name = "label1";
+            label1.Size = new Size(260, 31);
+            label1.TabIndex = 0;
+            label1.Text = "Welcome to Caro Chess";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Sitka Small", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(45, 103);
+            label2.Name = "label2";
+            label2.Size = new Size(74, 19);
+            label2.TabIndex = 1;
+            label2.Text = "Username";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Sitka Small", 9.75F);
+            label3.Location = new Point(45, 162);
+            label3.Name = "label3";
+            label3.Size = new Size(73, 19);
+            label3.TabIndex = 2;
+            label3.Text = "Password";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(126, 100);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(230, 23);
+            textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(126, 159);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(230, 23);
+            textBox2.TabIndex = 4;
+            textBox2.UseSystemPasswordChar = true;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.LightSteelBlue;
+            button1.Font = new Font("Sitka Small", 9.75F);
+            button1.Location = new Point(126, 227);
+            button1.Name = "button1";
+            button1.Size = new Size(100, 30);
+            button1.TabIndex = 5;
+            button1.Text = "Login";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.LightSteelBlue;
+            button2.Font = new Font("Sitka Small", 9.75F);
+            button2.Location = new Point(256, 227);
+            button2.Name = "button2";
+            button2.Size = new Size(100, 30);
+            button2.TabIndex = 6;
+            button2.Text = "Register";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.NavajoWhite;
+            ClientSize = new Size(431, 320);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Form2";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form2";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Button button1;
+        private Button button2;
+    }
+}
---
+++ Carotest/Form2.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Carotest
+{
+    public partial class Form2 : Form
+    {
+        private List<Account> accounts = new List<Account>();
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            {
+                string username = textBox1.Text;
+                string password = textBox2.Text;
+
+                // Kiểm tra xem tài khoản và mật khẩu có tồn tại trong danh sách không
+                if (IsLoginValid(username, password))
+                {
+                    MessageBox.Show("Login successfully!");
+
+                    // Mở Form1 và ẩn Form2
+                    Form1 form1 = new Form1();
+                    form1.Show();
+                    this.Hide();
+                }
+                else
+                {
+                    MessageBox.Show("Login failed. Please check your account and password again.");
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string username = textBox1.Text;
+            string password = textBox2.Text;
+
+            // Kiểm tra xem tên người dùng đã tồn tại chưa
+            if (IsUsernameAvailable(username))
+            {
+                // Tạo một tài khoản mới và thêm vào danh sách
+                Account newAccount = new Account { Username = username, Password = password };
+                accounts.Add(newAccount);
+                MessageBox.Show("Register successfully!");
+            }
+            else
+            {
+                MessageBox.Show("Username already exists. Please choose another name.");
+            }
+        }
+        private bool IsUsernameAvailable(string username)
+        {
+            foreach (var account in accounts)
+            {
+                if (account.Username == username)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        // Phương thức để kiểm tra xem tài khoản và mật khẩu có tồn tại hay không
+        private bool IsLoginValid(string username, string password)
+        {
+            foreach (var account in accounts)
+            {
+                if (account.Username == username && account.Password == password)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+    }
+    public class Account
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
